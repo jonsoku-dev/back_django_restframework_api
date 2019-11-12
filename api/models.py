4acb4269ff3cd338c76a138beb18ffa5f5316f3d
rakutenjongseok <whdtjr2792@gmail.com>
2019-11-13 06:44:50 +0900

    request data! wow~~~

--- api/models.py
+++ api/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User # 이미 user는 setting 되어 있으니 그냥 가져온다..
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.CharField(max_length=360)
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
