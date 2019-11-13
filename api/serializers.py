28a61f2294fa2dad4acef3ff0236b8d578b30820
rakutenjongseok <whdtjr2792@gmail.com>
2019-11-14 06:25:35 +0900

    customizing and token

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Movie, Rating
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,6 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
     # 무슨 의미?
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
 
 
