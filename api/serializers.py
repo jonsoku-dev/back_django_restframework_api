34e88e94e5a4e9929ef499501e4f5d642ee101e7
rakutenjongseok <whdtjr2792@gmail.com>
2019-11-14 06:03:55 +0900

    register user

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
 from .models import Movie, Rating
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only' : True, 'required' : True}}
+
+    # 무슨 의미?
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
